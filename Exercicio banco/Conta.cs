2e7d8497ddb76d90b3be73188181cfadeb8cf032
NunoJDMachado <njdmnuno7@hotmail.com>
2023-05-05 09:33:14 +0100

    criámos o projeto

---
+++ Exercicio banco/Conta.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_banco
+{
+    internal class Conta
+    {
+      
+        //Atributos
+        
+        private int _nconta;
+        private string _tconta;       
+        public double QuantiaDepInicial;
+        private double _saldo;
+        public char DepInicialResposta;
+
+        //Construtores
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int Nconta, string Tconta, double Saldo)
+        {
+            Nconta = _nconta;
+            Tconta = _tconta;
+            Saldo = _saldo;
+        }
+        
+        //Propriedades
+        public int Nconta
+        {
+            get
+            {
+                return _nconta;
+            }
+            set
+            {
+                _nconta = value;
+            }
+                 
+        }
+
+        public string Tconta
+        {
+            get
+            {
+                return _tconta;
+            }
+            set
+            {
+                if ()
+                {
+
+                }
+                _tconta = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+           
+        }
+
+
+       
+            
+           
+
+       
+        //Métodos
+        public void AumentarSaldo(double quantiaDep)
+        {
+            Saldo = Saldo + quantiaDep;
+        }
+
+        public void ReduzirSaldo(double quantiaLev)
+        {
+            Saldo = Saldo - quantiaLev - 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Nconta
+                + ", Titular: " + Tconta
+                + ", Saldo: " + +Saldo;
+        }
+    }
+}
---
+++ Exercicio banco/Program.cs
@@ -0,0 +1,107 @@
+﻿using Exercicio_banco;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Conta c1 = new Conta();
+
+        Console.Write("Insira o número da conta: ");
+        c1.Nconta = int.Parse(Console.ReadLine());
+        Console.Write("Insira o titular da conta: ");
+        c1.Tconta = Console.ReadLine();
+        Console.WriteLine("Haverá depósito inicial? s/n");
+        c1.DepInicialResposta = char.Parse(Console.ReadLine());
+        if (c1.DepInicialResposta == 's')
+        {
+            Console.Write("Insira o valor do depósito inicial:");
+            c1.QuantiaDepInicial = double.Parse(Console.ReadLine());
+            c1.Saldo = c1.QuantiaDepInicial;
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c1);
+            
+            Console.Write("Insira um valor para depósito: ");
+            double quantiaDep = double.Parse(Console.ReadLine());
+            c1.AumentarSaldo(quantiaDep);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c1);
+            
+            Console.Write("Insira um valor para levantamento: ");
+            double quantiaLev = double.Parse(Console.ReadLine());
+            c1.ReduzirSaldo(quantiaLev);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c1);
+        }   
+        else
+        {
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c1);
+
+            Console.Write("Insira um valor para depósito: ");
+            double quantiaDep = double.Parse(Console.ReadLine());
+            c1.AumentarSaldo(quantiaDep);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c1);
+
+            Console.WriteLine("Insira um valor para levantamento: ");
+            double quantiaLev = double.Parse(Console.ReadLine());
+            c1.ReduzirSaldo(quantiaLev);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(c1);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        /*Conta c1 = new Conta();
+
+        Console.Write("Insira o número da conta: ");
+        c1.Nconta = int.Parse(Console.ReadLine());
+        Console.Write("Insira o titular da conta: ");
+        c1.Tconta = Console.ReadLine();
+        Console.Write("Haverá depósito inicial? s/n");
+        c1.DepInicial = Console.ReadLine();
+        if (c1.DepInicial = 's')
+        {
+            Console.Write("Insira o valor do depósito inicial:");
+            c1.QuantiaDepInicial = double.Parse(Console.ReadLine());
+            c1.Saldo = c1.QuantiaDepInicial;
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+            Console.Write("Insira um valor para depósito: ");
+            c1.quantiaDep = double.Parse(Console.ReadLine());
+            c1.Saldo = c1.Saldo + c1.quantiaDep;
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+            Console.WriteLine("Insira um valor para levanamento: ");
+            c1.quantiaLev = double.Parse (Console.ReadLine());
+            c1.Saldo = c1.Saldo - c1.quantiaLev - 5.0;
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+        }
+        else
+        {
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+            Console.Write("Insira um valor para depósito: ");
+            c1.quantiaDep = double.Parse(Console.ReadLine());
+            c1.Saldo = c1.Saldo + c1.quantiaDep;
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+            Console.WriteLine("Insira um valor para levanamento: ");
+            c1.quantiaLev = double.Parse(Console.ReadLine());
+            c1.Saldo = c1.Saldo - c1.quantiaLev - 5.0;
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine("Conta " + c1.Nconta + ", Titular: " + c1.Tconta + ", Saldo: " + c1.Saldo + "€");
+        }*/
+    }
+}
